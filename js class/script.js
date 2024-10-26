2b08267b8b2b02f0a22015fa46c696f7fabbfd2b
TUBASKIAN <joeogbemisola72@gmail.xom>
2024-10-26 13:10:44 +0100

    added bootstrap project files

---
+++ js class/script.js
@@ -0,0 +1,7 @@
+let text1 = "Hello";
+let text2 = "world!";
+let text3 = "Have a nice day!";
+let result = text1.concat(" ", text2, " ", text3);
+
+document.getElementById("demo").innerHTML = result;
+console.log(result)
